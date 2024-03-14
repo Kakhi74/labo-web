04491145308f075d695a85998e91ddca75c266c0
Kakhi74 <kakhi1@ulaval.ca>
2024-03-14 19:31:13 -0400

    lab6

---
+++ labo6/lab6/src/components/Day.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="day">
+    <div class="weekday">
+      {{ day.weekday }}
+    </div>
+    <img :src="`https://www.weatherbit.io/static/img/icons/${day.icon}.png`" />
+    <div class="temperature">
+      <span style="color: grey">{{ day.high }}</span>
+      <span>{{ day.low }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DayItem",
+  props: ["day"],
+};
+</script>
